38c62eb572a04e7d51edbccc4e0a63df529577f4
Jose Vicente Gomes Filho <vicente.gomes.filho@gmail.com>
2026-01-05 19:00:36 +0100

    Fixed vite reference

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rna-seq-browser/', // REPLACE THIS with your repository name
+  base: '/Boldt-et-al-2022-reanalysis/',
 })
