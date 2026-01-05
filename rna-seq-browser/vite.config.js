5c43a77b29e3bc0cba78d5d0c6d7fc2aeaf98b88
Jose Vicente Gomes Filho <vicente.gomes.filho@gmail.com>
2026-01-05 18:36:37 +0100

    Initial Commit

---
+++ rna-seq-browser/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/rna-seq-browser/', // REPLACE THIS with your repository name
+})
