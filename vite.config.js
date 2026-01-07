423120a22ef11ebe546522747ed6524c6b9aa547
Jose Vicente Gomes Filho <vicente.gomes.filho@gmail.com>
2026-01-07 11:48:32 +0100

    Config fixed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Boldt-et-al-2022-reanalysis/',
+  base: '/Boldt-et-al-2023-reanalysis/',
 })
